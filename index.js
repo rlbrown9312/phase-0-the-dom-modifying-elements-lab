533ee1020453d59b50b67f9709e6bc90e39d5e2c
Richard <richard.brown0807@gmail.com>
2022-09-22 17:08:19 -0500

    commit tree ea2fdd67800d968dd620ea29e7ec99dda9e48a71

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-document.querySelector('main').remove();
-const newHeader = document.createElement('h1');
-newHeader.id = 'victory';
-newHeader.innerHTML = "YOUR-NAME is the champion"
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "YOUR-NAME is the champion"; //Or// newHeader.innerHtml
+
+
+
+//Removing Node Main
+// document.querySelector('main').remove();
+
+//Declaring newHeader as a variable and assigning it to the creation of a new h1 tag
+// const newHeader = document.createElement('h1');
+
+//assignment newHeader ID to 'victory'
+// newHeader.id = 'victory';
+
+//Modyfying text inside of h1 
+// newHeader.innerHTML = "YOUR-NAME is the champion"
